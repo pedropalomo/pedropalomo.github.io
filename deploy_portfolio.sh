979f182e6b5acacf6eba3915085b20cd043e1a3a
Pedro Palomo <pedro.palomoperez@gmail.com>
2017-05-16 20:54:22 +0000

    update sources

---
+++ deploy_portfolio.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+ 
+ 
+# build 
+gulp build
+ 
+# Update pedropalomo.github.io.src
+
+
+# Update pedropalomo.github.io
+cp -rf _site/* ../pedropalomo.github.io/
+ 
+cd ../pedropalomo.github.io/
+
+git add -A
+
+git commit -a -m "update sources"
+
+git push -u origin master
