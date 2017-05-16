3aed273b4074f9316fe85ad1f6e47fccf30a6e22
Pedro Palomo <pedro.palomoperez@gmail.com>
2017-05-16 21:03:05 +0000

    update sources

--- deploy_portfolio.sh
+++ deploy_portfolio.sh
@@ -1,19 +1,21 @@
 #!/bin/bash 
  
- 
-# build 
-gulp build
- 
-# Update pedropalomo.github.io.src
+if [ $1="-b" ]; then
+    # build 
+    gulp build
+else
+    # Update pedropalomo.github.io.src
+    
+    
+    # Update pedropalomo.github.io
+    cp -rf _site/* ../pedropalomo.github.io/
+     
+    cd ../pedropalomo.github.io/
+    
+    git add -A
+    
+    git commit -a -m "update sources"
+    
+    git push -u origin master
 
-
-# Update pedropalomo.github.io
-cp -rf _site/* ../pedropalomo.github.io/
- 
-cd ../pedropalomo.github.io/
-
-git add -A
-
-git commit -a -m "update sources"
-
-git push -u origin master
+fi
